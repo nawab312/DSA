cf35c86f88b452a8f87ed9833cdd9be8443269b1
Siddharth Singh <72911921+nawab312@users.noreply.github.com>
2025-01-10 17:27:46 +0530

    Update Reverse Vowels of String.py

--- String/Reverse Vowels of String.py
+++ String/Reverse Vowels of String.py
@@ -1,20 +1,2 @@
-class Solution(object):
-    def reverseVowels(self, s):
-        vowels = "aieouAIEOU"
-        s = list(s)
-        left = 0
-        right = len(s) - 1
 
-        while right >= left:
-            if s[left] not in vowels:
-                left += 1
-            elif s[right] not in vowels:
-                right -= 1
-            else:
-                s[left], s[right] = s[right], s[left]
-                left += 1
-                right -= 1
-        s = ''.join(s)
-        return s
-            
         
